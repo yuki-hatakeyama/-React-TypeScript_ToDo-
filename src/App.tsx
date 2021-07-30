4e7696d96310b781653a97ec21a8cff55d8a1360
yuki-hatakeyama <yuki-hatakeyama@two2.jp>
2021-07-30 19:15:58 +0900

    todoの削除機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,34 @@ import logo from "./logo.svg";
 
 const App = () => {
   const [todos, setTodos] = useState<string[]>([]);
-  const [todoItem, setTodoItem] = useState("");
+  const [todo, setTodo] = useState("");
 
-  const onRegistrationToDo = (event: any) => {
-    setTodos([...todos, todoItem]);
+  /*
+   * onRegistrationToDo()
+   * todoを登録するための関数
+   */
+  const onRegistrationToDo = () => {
+    if (todo.length > 0) {
+      setTodos([...todos, todo]);
+    }
+    setTodo(() => ""); // 初期化
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTodoItem(() => event.target.value);
+  /*
+   * getInputValue()
+   * inputBoxで入力した文字を取得するための関数
+   */
+  const getInputValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(() => event.target.value);
+  };
+
+  /*
+   * onDelete()
+   * 該当のtodoを削除するための関数
+   */
+  const onDelete = (index: number) => {
+    todos.splice(index, 1);
+    setTodos([...todos]);
   };
 
   return (
@@ -20,11 +40,23 @@ const App = () => {
       <section>
         <h1>TODO リスト</h1>
         <ul>
-          {todos.map((item: string) => {
-            return <li>{item}</li>;
+          {todos.map((item: string, index: number) => {
+            return (
+              <li key={index}>
+                <input type="checkbox" />
+                <span> {item}</span>
+                <button
+                  type="button"
+                  className="delete"
+                  onClick={() => onDelete(index)}
+                >
+                  削除
+                </button>
+              </li>
+            );
           })}
         </ul>
-        <input value={todoItem} onChange={handleChange} />
+        <input value={todo} onChange={getInputValue} />
         <button onClick={onRegistrationToDo}>登録</button>
       </section>
     </div>
